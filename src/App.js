4f9177b6603482e980f071b57b5b383381e3ff22
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-06-15 11:05:33 +0300

    Redux top level integration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
-
+import { Provider } from 'react-redux';
+import {store} from './utils/datasource'
 const browserHistory = createBrowserHistory();
 
 const App = () => {
   return (
+     <Provider store = {store}>
     <Router history={browserHistory}>
       <Routes />
     </Router>
+    </Provider>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
-export { default as Contact } from './contact';
-export { default as Subscribe } from './subscribe';
+
 export { default as RouteWithLayout } from './routeWithLayout';
