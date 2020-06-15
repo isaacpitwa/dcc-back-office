54ece9f7c7696d22470523c483974567649219e8
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-06-15 11:36:40 +0300

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+Powered by  Redux with files in  wrapped
+### `src/utils`
+and attached it to the `App.js`
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## Available Scripts 
+
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 import { Provider } from 'react-redux';
-import {store} from './utils/datasource'
+import { store } from './utils/datasource'
 const browserHistory = createBrowserHistory();
 
 const App = () => {
   return (
-     <Provider store = {store}>
-    <Router history={browserHistory}>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
     </Provider>
   );
 }
