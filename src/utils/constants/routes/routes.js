56f568663ad2f2626c665a88458e0668afa279fa
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-07-14 22:05:01 +0300

    route definition

--- src/utils/constants/routes/routes.js
+++ src/utils/constants/routes/routes.js
@@ -3,6 +3,42 @@ export const LANDING = '/';
 // dashboard routes
 export const DASHBOARD = '/dashboard';
 
+// sermon
+export const SERMONS = '/dashboard/sermons';
+export const NEW_SERMON = '/dashboard/sermons/new';
+
+// event
+export const EVENTS = '/dashboard/events';
+export const NEW_EVENT = '/dashboard/event/new';
+
+// event
+export const MINISTRIES = '/dashboard/minitries';
+export const NEW_MINISTRY = '/dashboard/ministries/new';
+
+
+// donations
+export const DONATIONS = '/dashboard/donantions';
+export const NEW_DONATION = '/dashboard/donations/new';
+
+// Ads
+export const ADS = '/dashboard/ads';
+export const NEW_AD = '/dashboard/ads/new';
+
+
+// Twitter
+export const TWITTER = '/dashboard/twitter';
+export const NEW_TWITTER = '/dashboard/twitter/new';
+
+// Twitter
+export const FACEBOOK = '/dashboard/facebook';
+export const NEW_FACEBOOK = '/dashboard/facebook/new';
+// Twitter
+export const INSTAGRAM = '/dashboard/instagram';
+export const NEW_INSTAGRAM = '/dashboard/instagram/new';
+
+// Utilities
+export const SETTINGS = '/dashboard/settings';
+
 
 // Auth routes
 export const LOGIN = '/auth/login';
